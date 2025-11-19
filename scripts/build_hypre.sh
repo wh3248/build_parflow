ed112ed1883bed5004c2c4aefcaf4b7d883d1080
wh3248 <user.name>
2025-11-19 17:40:25 -0500

    remove build files

--- scripts/build_hypre.sh
+++ scripts/build_hypre.sh
@@ -4,6 +4,6 @@
 
  mkdir -p $HYPRE_DIR
 cd $HYPRE_DIR
-curl -L https://github.com/hypre-space/hypre/archive/v2.17.0.tar.gz | tar --strip-components=1 -xzv && \
+curl -L https://github.com/hypre-space/hypre/archive/v2.33.0.tar.gz | tar --strip-components=1 -xzv && \
 cd src && ./configure --prefix=$HYPRE_DIR --with-MPI && \
 make install
